853ef6652489e86825b6e9d8bb4a3b74a0f6a94f
Leemike2012 <henrysun5440@gmail.com>
2022-10-24 22:32:15 +0900

    2022/10/24 22:32

--- src/main/java/com/image/experiments/ImageExperiments.java
+++ src/main/java/com/image/experiments/ImageExperiments.java
@@ -21,6 +21,14 @@ public class ImageExperiments {
         ix.MirrorImage(path+imageName);
         //4-2.图像上下翻转
         ix.ReverseImage(path+imageName);
+        //5.把彩⾊图的某个区域的透明度调高
+        ix.ImageOpacity(path+imageName,50);
+        //6.3x3,5x5,7x7模糊
+        //ix.Fuzzy(path+imageName,5);
+        //7.多张图片拼接
+        String[] fileList = new String[]{path+"diablo-R00.png",path+"diablo-0G0.png",path+"diablo-00B.png"};
+        ix.MergeImage(fileList,2);
+        //8.遍历图像, 使⽤kernel来计算像素值,代替⽬标像素点
     }
     public void RGBMethod(String imagePath) throws IOException {
         BufferedImage imageR = ImageIO.read(new File(imagePath));
@@ -85,6 +93,29 @@ public class ImageExperiments {
         }
         ImageIO.write(image, "png", new File(path+"diablo-Grey.png"));
     }
+
+
+    public void ImageZoom(String imagePath,Double proportion) throws IOException {
+        BufferedImage image = ImageIO.read(new File(imagePath));
+        //获取图像长宽
+        int width = image.getWidth();
+        int height = image.getHeight();
+        //获取图片的缩放【宽高都是*了缩放比例的再取整】
+        Image scaledInstance = image.getScaledInstance(Double.valueOf(width*proportion).intValue(),Double.valueOf(height * proportion).intValue(),Image.SCALE_DEFAULT);
+        //将Image类型转换成BufferedImage对象[BufferedImage.TYPE_INT_ARGB：表示具有8位RGBA颜色成分的整数像素的图像]
+        BufferedImage newImage = new BufferedImage(Double.valueOf(width*proportion).intValue(),Double.valueOf(height * proportion).intValue(),BufferedImage.TYPE_INT_ARGB);
+        // 一个新的图形上下文，这是这个图形上下文的副本
+        Graphics g = newImage.getGraphics();
+        // 绘制图片大小
+        boolean b = g.drawImage(scaledInstance, 0, 0, null);
+        // 释放文件资源
+        g.dispose();
+        // 将新的图片文件写入到指定的文件夹中
+        ImageIO.write(newImage,"png",new File(path+"zelda-zoom.png"));
+        System.out.println("缩放图片生成成功！");
+    }
+
+
     public void MirrorImage(String imagePath) throws IOException {
         BufferedImage imageV = ImageIO.read(new File(imagePath));
         //获取图像长宽
@@ -95,8 +126,7 @@ public class ImageExperiments {
                 int pl = imageV.getRGB(j, i);
                 int pr = imageV.getRGB(b, i);
                 imageV.setRGB(j, i, pr);
-                imageV.setRGB(b, i, pl);
-                
+                imageV.setRGB(b, i, pl);  
             }
         }  
         ImageIO.write(imageV, "png", new File(path+"diablo-LRreverse.png"));
@@ -116,24 +146,99 @@ public class ImageExperiments {
         } 
         ImageIO.write(imageRe, "png", new File(path+"diablo-UDreverse.png"));
     }
-    
-    public void ImageZoom(String imagePath,Double proportion) throws IOException {
+
+    public void ImageOpacity(String imagePath,int opacityValue) throws IOException {
         BufferedImage image = ImageIO.read(new File(imagePath));
         //获取图像长宽
         int width = image.getWidth();
         int height = image.getHeight();
-        //获取图片的缩放【宽高都是*了缩放比例的再取整】
-        Image scaledInstance = image.getScaledInstance(Double.valueOf(width*proportion).intValue(),Double.valueOf(height * proportion).intValue(),Image.SCALE_DEFAULT);
-        //将Image类型转换成BufferedImage对象[BufferedImage.TYPE_INT_ARGB：表示具有8位RGBA颜色成分的整数像素的图像]
-        BufferedImage newImage = new BufferedImage(Double.valueOf(width*proportion).intValue(),Double.valueOf(height * proportion).intValue(),BufferedImage.TYPE_INT_ARGB);
-        // 一个新的图形上下文，这是这个图形上下文的副本
-        Graphics g = newImage.getGraphics();
-        // 绘制图片大小
-        boolean b = g.drawImage(scaledInstance, 0, 0, null);
-        // 释放文件资源
-        g.dispose();
-        // 将新的图片文件写入到指定的文件夹中
-        ImageIO.write(newImage,"png",new File(path+"diablo-zoom.png"));
-        System.out.println("缩放图片生成成功！");
+
+        //透明图片文件
+        BufferedImage output = new BufferedImage(width ,height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = output.createGraphics();
+        output = g2.getDeviceConfiguration().createCompatibleImage(width, height, Transparency.TRANSLUCENT);
+        g2.dispose();
+        g2 = output.createGraphics();
+
+
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                int rgb = image.getRGB(i, j);
+                Color color = new Color(rgb);
+                Color newcolor = new Color(color.getRed(), color.getGreen(),color.getBlue(), opacityValue);
+                output.setRGB(i,j,newcolor.getRGB());
+            }
+        }
+        g2.setComposite(AlphaComposite.SrcIn);
+        g2.drawImage(image, 0, 0, width,height, null);
+        g2.dispose();
+        ImageIO.write(output, "png", new File(path+"diablo-Opacity.png"));
+    }
+
+
+    
+
+    public void MergeImage(String[] fileList,int type){
+        int len = fileList.length;
+        if (len < 1) {
+            throw new RuntimeException("图片数量小于1");
+        }
+        File[] src = new File[len];
+        BufferedImage[] images = new BufferedImage[len];
+        int[][] ImageArrays = new int[len][];
+        for (int i = 0; i < len; i++) {
+            try {
+                src[i] = new File(fileList[i]);
+                images[i] = ImageIO.read(src[i]);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            int width = images[i].getWidth();
+            int height = images[i].getHeight();
+            ImageArrays[i] = new int[width * height];
+            ImageArrays[i] = images[i].getRGB(0, 0, width, height, ImageArrays[i], 0, width);
+        }
+        int newHeight = 0;
+        int newWidth = 0;
+        for (int i = 0; i < images.length; i++) {
+            // 横向
+            if (type == 1) {
+                newHeight = newHeight > images[i].getHeight() ? newHeight : images[i].getHeight();
+                newWidth += images[i].getWidth();
+            } else if (type == 2) {// 纵向
+                newWidth = newWidth > images[i].getWidth() ? newWidth : images[i].getWidth();
+                newHeight += images[i].getHeight();
+            }
+        }
+        if (type == 1 && newWidth < 1) {
+            return;
+        }
+        if (type == 2 && newHeight < 1) {
+            return;
+        }
+
+        // 生成新图片
+        try {
+            BufferedImage ImageNew = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            int height_i = 0;
+            int width_i = 0;
+            for (int i = 0; i < images.length; i++) {
+                if (type == 1) {
+                    ImageNew.setRGB(width_i, 0, images[i].getWidth(), newHeight, ImageArrays[i], 0,
+                            images[i].getWidth());
+                    width_i += images[i].getWidth();
+                } else if (type == 2) {
+                    ImageNew.setRGB(0, height_i, newWidth, images[i].getHeight(), ImageArrays[i], 0, newWidth);
+                    height_i += images[i].getHeight();
+                }
+            }
+            //输出想要的图片
+            ImageIO.write(ImageNew, "png", new File(path+"diablo-merge.png"));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+    
